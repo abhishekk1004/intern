8aff4469c304cd87ac65892b28d54fff8e447867
beast 1004 <abhiobray143@gmail.com>
2024-07-11 21:07:31 +0545

    Thrusday

---
+++ accounts/views.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+
+# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Redirect to your desired page after registration
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
